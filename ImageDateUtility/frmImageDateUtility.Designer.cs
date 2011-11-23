45e35a3d09821cabee72ff225e55230a68216535
benkillin <github@b1towel.com>
2011-11-23 18:02:44 -0500

    initial commit

---
+++ ImageDateUtility/frmImageDateUtility.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ImageDateUtility
+{
+    partial class frmImageDateUtility
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.dlgOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.dlgFolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtBatchFilePath = new System.Windows.Forms.TextBox();
+            this.btnSelectSourceBatch = new System.Windows.Forms.Button();
+            this.dtpBatchTime = new System.Windows.Forms.DateTimePicker();
+            this.btnStartBatch = new System.Windows.Forms.Button();
+            this.dtpBatchDate = new System.Windows.Forms.DateTimePicker();
+            this.chkRandomize = new System.Windows.Forms.CheckBox();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.chkRandomize);
+            this.groupBox2.Controls.Add(this.dtpBatchDate);
+            this.groupBox2.Controls.Add(this.btnStartBatch);
+            this.groupBox2.Controls.Add(this.dtpBatchTime);
+            this.groupBox2.Controls.Add(this.btnSelectSourceBatch);
+            this.groupBox2.Controls.Add(this.txtBatchFilePath);
+            this.groupBox2.Controls.Add(this.progressBar1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(522, 128);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Batch Processing";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(6, 99);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(509, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // txtBatchFilePath
+            // 
+            this.txtBatchFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBatchFilePath.Location = new System.Drawing.Point(6, 19);
+            this.txtBatchFilePath.Name = "txtBatchFilePath";
+            this.txtBatchFilePath.Size = new System.Drawing.Size(374, 20);
+            this.txtBatchFilePath.TabIndex = 1;
+            // 
+            // btnSelectSourceBatch
+            // 
+            this.btnSelectSourceBatch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectSourceBatch.Location = new System.Drawing.Point(386, 17);
+            this.btnSelectSourceBatch.Name = "btnSelectSourceBatch";
+            this.btnSelectSourceBatch.Size = new System.Drawing.Size(128, 23);
+            this.btnSelectSourceBatch.TabIndex = 2;
+            this.btnSelectSourceBatch.Text = "Select Source Directory";
+            this.btnSelectSourceBatch.UseVisualStyleBackColor = true;
+            this.btnSelectSourceBatch.Click += new System.EventHandler(this.btnSelectSourceBatch_Click);
+            // 
+            // dtpBatchTime
+            // 
+            this.dtpBatchTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpBatchTime.Location = new System.Drawing.Point(212, 45);
+            this.dtpBatchTime.Name = "dtpBatchTime";
+            this.dtpBatchTime.ShowUpDown = true;
+            this.dtpBatchTime.Size = new System.Drawing.Size(100, 20);
+            this.dtpBatchTime.TabIndex = 3;
+            // 
+            // btnStartBatch
+            // 
+            this.btnStartBatch.Location = new System.Drawing.Point(6, 71);
+            this.btnStartBatch.Name = "btnStartBatch";
+            this.btnStartBatch.Size = new System.Drawing.Size(508, 23);
+            this.btnStartBatch.TabIndex = 4;
+            this.btnStartBatch.Text = "Start Processing";
+            this.btnStartBatch.UseVisualStyleBackColor = true;
+            this.btnStartBatch.Click += new System.EventHandler(this.btnStartBatch_Click);
+            // 
+            // dtpBatchDate
+            // 
+            this.dtpBatchDate.Location = new System.Drawing.Point(6, 45);
+            this.dtpBatchDate.Name = "dtpBatchDate";
+            this.dtpBatchDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpBatchDate.TabIndex = 5;
+            // 
+            // chkRandomize
+            // 
+            this.chkRandomize.AutoSize = true;
+            this.chkRandomize.Location = new System.Drawing.Point(318, 46);
+            this.chkRandomize.Name = "chkRandomize";
+            this.chkRandomize.Size = new System.Drawing.Size(157, 17);
+            this.chkRandomize.TabIndex = 6;
+            this.chkRandomize.Text = "Randomize time within day?";
+            this.chkRandomize.UseVisualStyleBackColor = true;
+            // 
+            // frmImageDateUtility
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 152);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "frmImageDateUtility";
+            this.Text = "Image Date Utility";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.OpenFileDialog dlgOpenFile;
+        private System.Windows.Forms.FolderBrowserDialog dlgFolderBrowser;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnSelectSourceBatch;
+        private System.Windows.Forms.TextBox txtBatchFilePath;
+        private System.Windows.Forms.DateTimePicker dtpBatchTime;
+        private System.Windows.Forms.Button btnStartBatch;
+        private System.Windows.Forms.DateTimePicker dtpBatchDate;
+        private System.Windows.Forms.CheckBox chkRandomize;
+    }
+}
+
---
+++ ImageDateUtility/frmImageDateUtility.cs
@@ -0,0 +1,181 @@
+﻿/**
+ * This program is for changing the EXIF date and the file create,mod,access times of JPG images.
+ * 
+ * Do not use this program for illegal, immoral, or otherwise nefarious purposes.
+ * 
+ * @author benkillin
+ * @date 22 Nov 2011
+ * 
+##    Copyright (C) 2011 benkillin
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##   You should have received a copy of the GNU General Public License
+##   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows;
+using System.Drawing.Imaging;
+using System.Threading;
+
+namespace ImageDateUtility
+{
+    public partial class frmImageDateUtility : Form
+    {
+        public const int EXIF_DATE_TIME = 306;
+        public const int EXIF_DATE_TIME_ORIGINAL = 36867;
+        public const int EXIF_DATE_TIME_DIGITIZED = 36868;
+
+        private string path;
+        private string exifDateString;
+        private DateTime date;
+        private DateTime time;
+        private DateTime combinedTime;
+        private DialogResult result;
+
+        public frmImageDateUtility()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectSourceBatch_Click(object sender, EventArgs e)
+        {
+            result = dlgFolderBrowser.ShowDialog();
+
+            txtBatchFilePath.Text = dlgFolderBrowser.SelectedPath;
+        }
+
+        private void btnStartBatch_Click(object sender, EventArgs e)
+        {
+            if (result.ToString().ToUpper().Equals("OK"))
+            {
+                DialogResult confirmResult = MessageBox.
+                    Show("ARE YOU SURE YOU WISH TO PROCESS THE PHOTOS IN THE DIRECTORY BELOW?\n\n" +
+                    this.path
+                    + "\n\nDoing so will re-encode the JPG files with a new file timestamp and modified EXIF data, meaning a slight loss of image information due to JPG compression (meaning the picture quality will degrade slightly). So don't do this more than once on a given directory and only do it on copies, not the original photos from your camera.",
+                    "Are you sure?", 
+                    MessageBoxButtons.YesNo, 
+                    MessageBoxIcon.Question);
+
+                if (confirmResult.ToString().ToUpper().Equals("YES"))
+                {
+                    path = dlgFolderBrowser.SelectedPath;
+                    date = dtpBatchDate.Value;
+                    time = dtpBatchTime.Value;
+                    combinedTime = new DateTime(date.Year, date.Month, date.Day, time.Hour, time.Minute, time.Second, time.Millisecond);
+
+                    Thread t = new Thread(new ThreadStart(processPhotos));
+                    t.Start();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error: Selecting the batch processing folder was unsuccessful.\nPlease click the 'Select Source Directory' button and select a directory to start processing photos from.", "Error processing photos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// The worker function for patch processing. This is to be started as a separate thread.
+        /// </summary>
+        private void processPhotos()
+        {
+            Random r = new Random((int)DateTime.Now.Ticks);
+            List<string> files = new List<string>();
+
+            files.AddRange(Directory.GetFiles(path, "*.jpg"));
+            files.AddRange(Directory.GetFiles(path, "*.jpeg"));
+            
+            this.BeginInvoke((Action)(() => progressBar1.Value = 0));
+            this.BeginInvoke((Action)(() => progressBar1.Step = 1));
+            this.BeginInvoke((Action)(() => progressBar1.Maximum = files.Count + 1));
+            this.BeginInvoke((Action)(() => progressBar1.PerformStep())); // do one progress bar step immediately to indicate we have started processing
+
+            foreach (string file in files)
+            {
+                exifDateString = combinedTime.ToString("yyyy:MM:dd HH:mm:ss");
+
+                // set EXIF data to specified time
+                // http://www.media.mit.edu/pia/Research/deepview/exif.html
+                //0x0132 	DateTime 	ascii string 	20 	Date/Time of image was last modified. Data format is "YYYY:MM:DD HH:MM:SS"+0x00, total 20bytes. In usual, it has the same value of DateTimeOriginal(0x9003)
+                //0x9003 	DateTimeOriginal 	ascii string 	20 	Date/Time of original image taken. This value should not be modified by user program.
+                //0x9004 	DateTimeDigitized 	ascii string 	20 	Date/Time of image digitized. Usually, it contains the same value of DateTimeOriginal(0x9003).
+                // http://archive.msdn.microsoft.com/changexifwithcsharp
+                Image iOrig = new Bitmap(file);
+                ImageFormat ifOrig = iOrig.RawFormat;
+                PropertyItem exifDateTimeProp = iOrig.GetPropertyItem(EXIF_DATE_TIME);
+                PropertyItem exifDateTimeOrigProp = iOrig.GetPropertyItem(EXIF_DATE_TIME_ORIGINAL);
+                PropertyItem exifDateTimeDigiProp = iOrig.GetPropertyItem(EXIF_DATE_TIME_DIGITIZED);
+
+                exifDateTimeProp.Value = System.Text.Encoding.UTF8.GetBytes(exifDateString);
+                exifDateTimeOrigProp.Value = System.Text.Encoding.UTF8.GetBytes(exifDateString);
+                exifDateTimeDigiProp.Value = System.Text.Encoding.UTF8.GetBytes(exifDateString);
+
+                iOrig.SetPropertyItem(exifDateTimeProp);
+                iOrig.SetPropertyItem(exifDateTimeOrigProp);
+                iOrig.SetPropertyItem(exifDateTimeDigiProp);
+
+                Image iSave = new Bitmap(iOrig.Width, iOrig.Height, iOrig.PixelFormat);
+                Graphics gSave = Graphics.FromImage(iSave);
+
+                gSave.DrawImage(iOrig, 0, 0, iOrig.Width, iOrig.Height);
+
+                foreach (PropertyItem i in iOrig.PropertyItems)
+                    iSave.SetPropertyItem(i);
+
+                // WHAT THE FUCK?!
+                // You *HAVE* to dispose these two objects, iOrig and gSave, BEFORE you save iSave otherwise
+                // you'll get an unexplained GDI+ exception... THANK YOU MICROSOFT! >:(
+                iOrig.Dispose();
+                gSave.Dispose();
+
+                iSave.Save(file, ifOrig);
+
+                iSave.Dispose();
+
+
+                // set file create, mod, and access time to the specified time
+                // (this has to be done after the EXIF modification because that will modify the file, of course)
+
+                File.SetCreationTime(file, combinedTime);
+                File.SetLastWriteTime(file, combinedTime);
+                File.SetLastAccessTime(file, combinedTime);
+
+
+                if (chkRandomize.Checked)
+                { // randomize the time to between 0800 and 1600 to make it believable:
+                    int hour = r.Next(8, 17);
+                    int minute = r.Next(60);
+                    int second = r.Next(60);
+                    int millisecond = r.Next(1000);
+
+                    combinedTime = new DateTime(combinedTime.Year, combinedTime.Month, combinedTime.Day, hour, minute, second, millisecond);
+                }
+
+
+                // do progressbar step after we finished a file to give accurate feedback
+                this.BeginInvoke((Action)(() => progressBar1.PerformStep()));
+            }
+
+
+            MessageBox.Show("Finished Processing.", "Success");
+            this.BeginInvoke((Action)(() => progressBar1.Value = 0));
+        }
+    }
+}
